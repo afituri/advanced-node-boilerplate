a95fae9e90e0edafdf4038f246e601872f822dd5
Ahmed Fituri <ahmed.elfituri@gmail.com>
2019-10-21 22:29:45 +0200

    Remove uneeded routes

--- api/users/router.js
+++ api/users/router.js
@@ -1,17 +1,8 @@
-const express = require("express");
-const controller = require("./controller");
+const express = require('express');
+const controller = require('./controller');
 
 const router = express.Router();
 
-router.route("/").post(controller.usersCreate);
-
-// router.route("/login").post(controller.loginUser);
-
-// router.route("/me").get(isUser, controller.me);
-
-// router
-//   .route("/:id")
-//   .get(isUser, controller.usersShow)
-//   .put(controller.usersUpdate);
+router.route('/').post(controller.usersCreate);
 
 module.exports = router;
